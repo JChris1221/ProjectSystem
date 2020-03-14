e35fbf6557d3de15bdcdadca6b1cfc5bd19b43f4
John Chris Pingkian <jcpingkian@gmail.com>
2020-03-14 15:01:07 +0800

    Change criteria table records

--- thesis-grading-system-db.sql
+++ thesis-grading-system-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2020 at 05:43 PM
+-- Generation Time: Mar 14, 2020 at 08:00 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -57,6 +57,10 @@ INSERT INTO `accounts` (`Id`, `Username`, `Password`, `Firstname`, `Lastname`, `
 CREATE TABLE `criteria` (
   `Id` int(11) NOT NULL,
   `Title` varchar(255) NOT NULL,
+  `Beginner` varchar(255) NOT NULL,
+  `Acceptable` varchar(255) NOT NULL,
+  `Proficient` varchar(255) NOT NULL,
+  `Exemplary` varchar(255) NOT NULL,
   `Weight` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -64,21 +68,22 @@ CREATE TABLE `criteria` (
 -- Dumping data for table `criteria`
 --
 
-INSERT INTO `criteria` (`Id`, `Title`, `Weight`) VALUES
-(1, 'Content', 0.066),
-(2, 'Organization', 0.066),
-(3, 'Use of Reference', 0.066),
-(4, 'Tone', 0.066),
-(5, 'Instructional Content 1', 0.066),
-(6, 'Instructional Content 2', 0.066),
-(7, 'Instructional Content 3', 0.066),
-(8, 'Instructional Content 4', 0.066),
-(9, 'Instructional Content 5', 0.066),
-(10, 'Layout 1', 0.066),
-(11, 'Layout 2', 0.066),
-(13, 'Layout 3', 0.066),
-(14, 'Layout 4', 0.066),
-(15, 'Complete', 0.066);
+INSERT INTO `criteria` (`Id`, `Title`, `Beginner`, `Acceptable`, `Proficient`, `Exemplary`, `Weight`) VALUES
+(1, 'Content', 'Central purpose or argument is not clearly identified Analysis is Vague or not evident. Reader is confused or may be misinformed', 'Information Supports central purpose or argument at times. Analysis is basic or general. Reader gains few insights', 'Information provides reasonable support for a central purpose or argument and displays evidence of a basic analysis of a significant topic Reader gains some insights.', 'Balanced presentation of relevant information that clearly supports a central purpose or argument and shows a thoughtful, in-depth, analysis of a significant topic. Reader gains important insights.', 0.066),
+(2, 'Organization', 'The writing is not logically organized. Frequently, ideas fail to make sense together. The reader cannot identify a line of reasoning and loses interest', 'In general, the writing is arranged logically, although occasionally ideas fail to make sense together. The reader is fairly clear about what the writer intends.', 'The ideas are arranged logically to support the central purpose or argument. They are usually clearly linked to each other. For the most part the reader can follow the line of reasoning', 'The ideas are arranged logically to support the purpose or argument. They flow smoothly from one to another and are clearly linked to each other. The reader can follow the line of reasoning', 0.066),
+(3, 'Use of Reference', 'References are seldom cited to support statements.', 'Although attribution is occasionally given, many statements seem unsubstantiated. The reader is confused about the source of information ideas.', 'Professionally legitimate sources that supports the claims are generally present and attribution is, for the most part, clear and fairly represented', 'Compelling evidence from professionally legitimate sources is given to support claims. Attribution is clear and represented', 0.066),
+(4, 'Language Conventions', 'There are many grammatical errors.', 'Periodic grammatical errors.', 'There are occasional grammatical errors.', 'The writing is free or almost free of errors', 0.066),
+(5, 'Tone', 'The tone is unprofessional. It is not appropriate for an academic research paper.', 'The tone is not consistently professional or appropriate for an academic research paper.', 'The tone is generally professional. For the most part, it is appropriate for an academic research paper.', 'The tone is consistently professional and appropriate for an academic research paper', 0.066),
+(6, 'Instructional Content 1', 'Information is inaccurate, incomplete or outdated.', 'Information is not always accurate, complete or current.', 'Information is accurate and most is complete and current', 'Information is accurate, complete and current', 0.066),
+(7, 'Instructional Content 2', 'Facts do not come from reliable sources or sources are not identified.', 'Facts come from questionable sources.', 'Facts usually come from reliable sources which are clearly identified.', 'Facts usually come from reliable sources which are clearly identified.', 0.066),
+(8, 'Instructional Content 3', 'Little or no overall context for information.', 'Content is note related to larger context', 'Content is usually related to larger context', 'Content and context are consistent with the theme', 0.066),
+(9, 'Instructional Content 4', 'Purpose is unclear.', 'Content lacks sense of purpose or central theme.', 'General purpose is identified.', 'All information relates to the stated purpose and learning goals.', 0.066),
+(10, 'Instructional Content 5', 'Content focuses entirely on fundamental concepts, rote memory, or recitation of facts; no provision for moving students to higher levels of thinking by applying what is learned', 'Content focuses on fundamental concepts and rarely engages students in higher levels of thinking; students are rarely asked to apply what they learn.', 'Content provides some activities which encourage higher levels of thinking; students are frequently asked to apply what they have learned', 'Content moves learners beyond the basics and encourages higher levels of thinking; students are engaged in applying what they learn', 0.066),
+(11, 'Layout 1', 'Layout is confusing.', 'Layout is not intuitive.', 'Layout is clear but learners need help to find necessary features.', 'Layout is clear and intuitive; learners can always find what they need', 0.066),
+(12, 'Layout 2', 'Learners cannot navigate through the information to find what they need', 'Layout is difficult to navigate', 'Learners can usually navigate through the information to find what they need.', 'It is easy to navigate through the information to find necessary features.', 0.066),
+(13, 'Layout 3', 'Layout is illogical and unpredictable.', 'Layout is frequently illogical.', 'Layout is logical in most cases, but sometimes confusing.', 'Layout is logical.', 0.066),
+(14, 'Layout 4', 'Layout is inconsistent.', 'Layout is frequently inconsistent.', 'Layout is frequently consistent but occasionally confusing.', 'Layout is consistent on all pages.', 0.066),
+(15, 'Complete', 'Several of the problems are not completed.', 'All but two of the problems are completed.', 'All but one of the problems are completed.', 'All problems are completed.', 0.066);
 
 -- --------------------------------------------------------
 
@@ -253,7 +258,8 @@ ALTER TABLE `faculty_types`
 --
 ALTER TABLE `grades`
   ADD PRIMARY KEY (`Panelist_Id`,`Group_Id`),
-  ADD KEY `Group_Id` (`Group_Id`);
+  ADD KEY `fk_groups_id` (`Group_Id`),
+  ADD KEY `grades_ibfk_3` (`Criteria_Id`);
 
 --
 -- Indexes for table `groups`
@@ -319,7 +325,8 @@ ALTER TABLE `faculty_assignment`
 --
 ALTER TABLE `grades`
   ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`Group_Id`) REFERENCES `groups` (`Id`),
-  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`Panelist_Id`) REFERENCES `accounts` (`Id`);
+  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`Panelist_Id`) REFERENCES `accounts` (`Id`),
+  ADD CONSTRAINT `grades_ibfk_3` FOREIGN KEY (`Criteria_Id`) REFERENCES `criteria` (`Id`);
 
 --
 -- Constraints for table `students`

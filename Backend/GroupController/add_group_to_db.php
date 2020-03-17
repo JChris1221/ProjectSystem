845ee414f044f72c0475232362e92c93ffd2881a
John Chris Pingkian <jcpingkian@gmail.com>
2020-03-17 13:40:05 +0800

    Added Add Group Validation

--- Backend/GroupController/add_group_to_db.php
+++ Backend/GroupController/add_group_to_db.php
@@ -43,6 +43,24 @@ else{
 	$panel_chair = $_POST["PanelChairId"];
 	$adviser = $_POST["AdviserId"];
 
+	if($profid == $adviser){
+		$_SESSION["AddGroupError"]	= "Professor and Adviser cannot be the same person";
+		header("Location: ../../GroupManagement/AddGroup.php");
+		exit();
+	}
+
+	if(in_array($adviser, $panelist) || $adviser == $panel_chair){
+		$_SESSION["AddGroupError"]	= "The adviser cannot be one of the panelist";
+		header("Location: ../../GroupManagement/AddGroup.php");
+		exit();
+	}
+
+	if(in_array($panel_chair, $panelist) || HasDuplicates($panelist)){
+		$_SESSION["AddGroupError"]	= "Same panels detected";
+		header("Location: ../../GroupManagement/AddGroup.php");
+		exit();
+	}
+
 	$students = array();
 	foreach($stud_fname as $key=>$value){
 
@@ -54,5 +72,12 @@ else{
 		header("Location: ../../GroupManagement/ManageGroups.php");
 	else
 		die("Error Adding Group");
+}
+
+function HasDuplicates($arr){
+	if(count($arr) != count(array_unique($arr)))
+		return true;
+	else
+		return false;
 }
 ?>
